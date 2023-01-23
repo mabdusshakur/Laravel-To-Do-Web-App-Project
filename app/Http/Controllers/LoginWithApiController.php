3c924dbe9cca6d13185307e72bed9ebd5d7701a5
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-01-23 22:03:21 +0600

    cURL error 28 , when hit /withapi/login .

--- app/Http/Controllers/LoginWithApiController.php
+++ app/Http/Controllers/LoginWithApiController.php
@@ -10,25 +10,27 @@ class LoginWithApiController extends Controller
 {
     public function index(Request $request)
     {
-        $http = new \GuzzleHttp\Client();
+        // $client = new \GuzzleHttp\Client();
+        // // $response = $http->post('http://127.0.0.1:8000/api/login', [
+        // //     'form_params' => [
+        // //         'email' => 'panda12@gmail.com',
+        // //         'password' => 'panda1221',
+        // //     ]
+        // // ]);
 
-        $response = $http->post('http://127.0.0.1:8000/api/login', [
-            'form_params' => [
-                'email' => 'panda12@gmail.com',
-                'password' => 'panda1221',
-            ]
-        ]);
+        // // return "ddd";
+        // $client = new \GuzzleHttp\Client();
+        // $response = $client->request('GET', 'http://127.0.0.1:8000/api/users');
+        // return "dd";
 
-        return "ddd";
+        return Http::get('http://127.0.0.1:8000/api/users');
     }
 
     public function register(Request $request)
     {
-
-
-        Http::post('http://127.0.0.1:8000/api/users', [
-            'email' => $request->get('email'),
-            'password' => Hash::make($request->get('password'))
-        ]);
+        // Http::post('http://127.0.0.1:8000/api/users', [
+        //     'email' => $request->get('email'),
+        //     'password' => Hash::make($request->get('password'))
+        // ]);
     }
 }
